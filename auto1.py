fe788fd3fe1805fe6f2082c8d99da98c8c11664e
zs3189 <810909753.qq.com>
2017-07-26 15:46:55 +0800

    测试查找

--- auto1.py
+++ auto1.py
@@ -4,3 +4,15 @@
 @contact: 810909753@q.com
 @time: 2017/7/26 15:36
 '''
+import pyautogui as pg
+
+image=pg.screenshot("1.png",region=(800,800,20,20))
+
+import time
+
+a=time.clock()
+qa=pg.locateOnScreen(image)
+
+print(qa)
+b=time.clock()
+print(b-a)
